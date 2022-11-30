8944a0af97686a4e43dfbaa7df6ba56cb6bfdcf0
J. Victor <49132641+LordDarkn3ss@users.noreply.github.com>
2022-11-30 14:56:17 -0300

    Update atv0001.java

--- EstudosArray/src/scripts/atv0001.java
+++ EstudosArray/src/scripts/atv0001.java
@@ -5,7 +5,7 @@ public class atv0001 {
 	public static void main(String[] args) {
 		for(int i = 0; i < 100; i++) {
 			if(i > 50 && i < 60) {
-				continue;
+				continue; // Vai pular tudo que for maior que 50 e menor que 60
 			}
 			System.out.println(i);
 			
